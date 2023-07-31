f58480ab3e172920c84821c8dfad23c7e038395b
f.beyribey <efekarapinar1@gmail.com>
2023-07-31 13:23:31 +0300

    v1.0 Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import './App.css';
+import Content from './Components/Content';
+import Sidebar from './Components/Sidebar';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
+import { GlobalStateProvider } from "./utils/GlobalStateProvider";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-    
+    <div className='App'>
+      <BrowserRouter>
+          <GlobalStateProvider>
+              <ToastContainer />
+              <Sidebar />
+            <Routes>
+              <Route path='/' element={<Content />} />
+              <Route path='/project/:projectId' element={<Content />} />
+            </Routes>
+          </GlobalStateProvider>
+      </BrowserRouter>
     </div>
   );
 }
-
-export default App;
